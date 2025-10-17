a7bd1b532e3954f89a191a1b38a3782cc238a533
Kristóf <foti.kristof@ganziskola.hu>
2025-10-17 07:56:29 +0200

    Képek létrehozva, elnevezve. File-ok létrehozva, valamint navigációs sáv.

---
+++ script.js
@@ -0,0 +1,111 @@
+
+
+const ELEMENTAL_CRAFT_STICKERS = [
+    {
+        név: "Sticker High Heat",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_High_Heat",
+        csillag: 1,
+        esély: 0.07142857142857142,
+        érték: 2.0
+    },
+    {
+        név: "Sticker Soft Charge",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Soft_Charge",
+        csillag: 1,
+        esély: 0.07142857142857142,
+        érték: 0.70
+    },
+    {
+        név: "Sticker Beam Trail",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Beam_Trail",
+        csillag: 1,
+        esély: 0.07142857142857142,
+        érték: 0.17
+    },
+    {
+        név: "Sticker Speed-Large",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Speed-Large",
+        csillag: 1,
+        esély: 0.07142857142857142,
+        érték: 0.28
+    },
+    {
+        név: "Sticker Green Trail",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Green_Trail",
+        csillag: 1,
+        esély: 0.03484320557491289,
+        érték: 0.46
+    },
+    {
+        név: "Sticker Beam Detention",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Beam_Detention",
+        csillag: 1,
+        esély: 0.03484320557491289,
+        érték: 0.20
+    },
+    {
+        név: "Sticker Belt Strike",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Strike",
+        csillag: 1,
+        esély: 0.03484320557491289,
+        érték: 0.30
+    },
+    {
+        név: "Sticker Belt Energy",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Energy",
+        csillag: 1,
+        esély: 0.03484320557491289,
+        érték: 0.18
+    },
+    {
+        név: "Sticker Belt Charge",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Charge",
+        csillag: 1,
+        esély: 0.026246719160104987,
+        érték: 5.81
+    },
+    {
+        név: "Sticker Belt Strike Full",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Strike_Full",
+        csillag: 1,
+        esély: 0.026246719160104987,
+        érték: 3.84
+    },
+    {
+        név: "Sticker Belt Energy Full",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Energy_Full",
+        csillag: 1,
+        esély: 0.026246719160104987,
+        érték: 2.83
+    },
+    {
+        név: "Sticker Body Wave Lambrake",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Body_Wave_Lambrake",
+        csillag: 1,
+        esély: 0.02036659877800407,
+        érték: 10.48
+    },
+    {
+        név: "Sticker Body Stress Lambrake",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Body_Stress_Lambrake",
+        csillag: 1,
+        esély: 0.02036659877800407,
+        érték: 10.49
+    },
+    {
+        név: "Sticker Body Wave Full",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Body_Wave_Full",
+        csillag: 1,
+        esély: 0.1694915254237288,
+        érték: 5.91
+    },
+    {
+        név: "Sticker Belt Charge Full",
+        kép: "kepek\\ELEMENTAL_CRAFT_STICKERS\\Sticker_Belt_Charge_Full",
+        csillag: 1,
+        esély: 0.1694915254237288,
+        érték: 2.89
+    }
+];
+
+
